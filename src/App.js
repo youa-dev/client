a468c7ae908d505cfb292acee920c886430a1606
Denis Onder <denis_onder@protonmail.com>
2020-04-11 16:41:26 +0200

    Very basic landing page and logo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from "react";
 import Navbar from "./components/imports/Navbar";
 import Home from "./components/pages/Home";
-import purple from "@material-ui/core/colors/purple";
-import green from "@material-ui/core/colors/green";
-import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-
-const theme = createMuiTheme({
-  palette: {
-    primary: purple,
-    secondary: green,
-  },
-  status: {
-    danger: "orange",
-  },
-});
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <div>
       <Navbar />
       <Home />
-    </ThemeProvider>
+    </div>
   );
 }
 
--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,14 +1,34 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 import "./index.scss";
 
 export default function Home() {
   return (
     <Container>
       <div className="home">
-        <h1 className="home_title">youa.dev</h1>
-        <h3 className="home_subtitle">A networking hub for developers.</h3>
+        <div className="home_text">
+          <div className="home_text--wrapper">
+            <div className="home_text--logo"></div>
+            <h1 className="home_text--title">youa.dev</h1>
+          </div>
+          <h3 className="home_text--subtitle">
+            A networking hub for developers.
+          </h3>
+        </div>
         <div className="home_svg"></div>
+        <div className="home_cta">
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            endIcon={<ArrowForwardIosIcon />}
+            style={{ marginTop: "15px" }}
+          >
+            Let's get started
+          </Button>
+        </div>
       </div>
     </Container>
   );
