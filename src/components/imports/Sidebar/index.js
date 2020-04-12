beb0031086392455aa6a3ab089f1b4213a5d0f85
Denis Onder <denis_onder@protonmail.com>
2020-04-12 15:27:32 +0200

    Setting up a sidebar and dashboard

---
+++ src/components/imports/Sidebar/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  AddCircleRounded,
+  Chat,
+  AccountCircle,
+  Search,
+  Settings,
+} from "@material-ui/icons";
+import "./style.scss";
+
+export default function Sidebar() {
+  return (
+    <nav className="sidebar">
+      <ul className="sidebar_icons_list">
+        <li className="sidebar_icons_list_icon">
+          <AccountCircle fontSize="inherit" />
+        </li>
+        <li className="sidebar_icons_list_icon">
+          <Chat fontSize="inherit" />
+        </li>
+        <li className="sidebar_icons_list_icon">
+          <AddCircleRounded fontSize="inherit" />
+        </li>
+        <li className="sidebar_icons_list_icon">
+          <Search fontSize="inherit" />
+        </li>
+        <li className="sidebar_icons_list_icon">
+          <Settings fontSize="inherit" />
+        </li>
+      </ul>
+    </nav>
+  );
+}
--- src/components/pages/Dashboard/index.js
+++ src/components/pages/Dashboard/index.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Container } from "@material-ui/core";
+import Sidebar from "../../imports/Sidebar";
+import "./style.scss";
 
 export default function Dashboard() {
   return (
-    <div>
-      Dashboard
-      <a href="/">Go back home</a>
-    </div>
+    <Fragment>
+      <Sidebar />
+      <Container>
+        <div className="dashboard">
+          <h1 className="dashboard_title">Dashboard</h1>
+          <a href="/">Go back home</a>
+        </div>
+      </Container>
+    </Fragment>
   );
 }
