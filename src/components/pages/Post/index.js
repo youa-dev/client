b34ddd891d51837077128020c6d51c09833c33bc
Denis Onder <denis_onder@protonmail.com>
2020-05-08 21:46:40 +0200

    Rendering sanitized HTML

--- src/components/pages/Post/index.js
+++ src/components/pages/Post/index.js
@@ -1,36 +1,6 @@
 import React, { Fragment, Component } from "react";
 import axios from "axios";
 import urlGenerator from "../../../helpers/urlGenerator";
-import { Redirect } from "react-router-dom";
-
-// export default function Post({ match }) {
-//   const forceUpdate = useForceUpdate();
-//   const handle = match.params.handle;
-//   const [post, setPost] = useState({});
-//   const [error, setError] = useState(null);
-//   useEffect(() => {
-//     (async () => {
-//       try {
-//         setPost(await axios.get(urlGenerator("posts", `/posts/get/${handle}`)));
-//       } catch (err) {
-//         setError(err);
-//       } finally {
-//         forceUpdate();
-//       }
-//     })();
-//   }, []);
-//   return error ? (
-//     <Redirect to="/404" />
-//   ) : (
-//     <Fragment>
-//       {post ? (
-//         <div className="post">
-//           <p className="post_title">{post.title}</p>
-//         </div>
-//       ) : null}
-//     </Fragment>
-//   );
-// }
 
 export default class Post extends Component {
   constructor(props) {
@@ -57,7 +27,10 @@ export default class Post extends Component {
       return (
         <div className="post">
           <h3 className="post_title">{post.title}</h3>
-          <div className="post_body">{post.body}</div>
+          <div
+            className="post_body"
+            dangerouslySetInnerHTML={{ __html: post.body }}
+          />
         </div>
       );
     }
